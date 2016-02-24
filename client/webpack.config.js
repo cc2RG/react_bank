c04c44686f8262b6863c05c486f88d53dacad38a
cc2RG <rbgalloway88@gmail.com>
2016-02-24 11:13:52 +0000

    first commit

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+var config = {
+  entry: "./src/index.js",
+  output:{
+    filename: "bundle.js",
+    path: "./build"
+  },
+  resolve:{
+    extensions: ['','.js','.jsx']
+  },
+  module:{
+    loaders:[
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader:'babel',
+        query:{
+          presets:['react','es2015']
+        }
+      }
+      
+    ]
+  },
+  devtool: "source-map"
+}
+
+module.exports = config;
